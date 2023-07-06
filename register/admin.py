acf19fe3ae8e70a30f1a5fb65195228056f765bf
jahon0088 <127406327+jahon0088@users.noreply.github.com>
2023-07-06 15:18:32 +0500

    User added to admin.py

--- register/admin.py
+++ register/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from register.models import Product
+from register.models import Product, User
 
+admin.site.register(User)
 admin.site.register(Product)
